ff7fd6ff230ea481e7c2f17c3872d7dfca96b6ed
shmaiii <luongxuanmai2109@gmail.com>
2024-01-20 14:36:13 -0800

    stub api

--- backend-express/index.js
+++ backend-express/index.js
@@ -8,8 +8,16 @@ app.use(express.json());
 app.use(cors());
 
 //APIS
+
+//example endpoint
 app.get("/app/helloworld", (req, res) => {
     res.send("Hello World");
+})
+
+app.get("/app/get_prophecy", (req, res) => {
+    // this is a stub api to get prophecy
+    // TODO: implement
+    res.send('You are pretty af');
 })
 
 const PORT = process.env.PORT || 30001;
