6bc04eccc6b0817d246c523d540090bb0d268476
linhtk <linh.kl.trinh@gmail.com>
2024-01-20 17:54:24 -0800

    Update App.css

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -9,12 +9,52 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import splash from './splash.gif';
+import './App.css'
 
 function SplashScreen() {
   return (
     <div className="splash-screen">
       <img src={splash} alt="Splash" style={{ width: '100%', height: '100%', objectFit: 'cover' }}/>
     </div>
+  );
+}
+
+function LandingPage({ onStart }) {
+  return (
+    <Container>
+      <Navbar>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Face Reading
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Detect
+            </Nav.Link>
+            <Nav.Link as={Link} to="/result">
+              Result
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="mt-3 text-center">
+        <h2>Welcome to FaceFuture!</h2>
+        <p>
+          Are you ready to explore the fascinating world where cutting-edge facial analysis meets
+          ancient divination? 
+        </p>
+        <p>
+          Let your face be the guide to unlocking the mysteries that lie ahead.
+        </p>
+        <p>
+          Let the adventure begin, and let your face tell the tale!
+        </p>
+        <Button variant="dark" onClick={onStart} className="mb-3">
+          Start
+        </Button>
+      </div>
+    </Container>
   );
 }
 
@@ -69,7 +109,7 @@ function CapturePage({ onCapture }) {
           <Webcam ref={webcamRef} />
         </Form.Group>
         <Form.Group className="mb-3 text-center">
-          <Button variant="primary" onClick={captureHandler} className="mb-3">
+          <Button variant="dark" onClick={captureHandler} className="mb-3">
             Capture
           </Button>
         </Form.Group>
@@ -100,7 +140,7 @@ function ResultPage({ resultData }) {
       <div className="mt-3 text-center">
         <h2>Result</h2>
         {resultData.image && <img src={resultData.image} alt="Result" className="mb-3" />}
-        <p>{resultData.additionalInfo}</p>
+        <p>{resultData}</p>
       </div>
     </Container>
   );
@@ -109,6 +149,7 @@ function ResultPage({ resultData }) {
 function App() {
   const [resultData, setResultData] = useState(null);
   const [showSplash, setShowSplash] = useState(true);
+  const [showLanding, setShowLanding] = useState(true);
 
   useEffect(() => {
     const splashTimeout = setTimeout(() => {
@@ -118,10 +159,16 @@ function App() {
     return () => clearTimeout(splashTimeout);
   }, []);
 
+  const startDetection = () => {
+    setShowLanding(false);
+  };
+
   return (
     <Router>
       {showSplash ? (
         <SplashScreen />
+      ) : showLanding ? (
+        <LandingPage onStart={startDetection} />
       ) : (
         <Routes>
           <Route path="/" element={<CapturePage onCapture={setResultData} />} />
